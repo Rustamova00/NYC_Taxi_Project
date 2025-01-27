bf30acd0b7d53fa46a06f0876fcbe173992d5f92
Khilola Rustamova <99622577+Rustamova00@users.noreply.github.com>
2025-01-27 02:22:52 -0500

    Update TLCDriver_2024_01_dash_to_streamlit.py

--- TLCDriver_2024_01_dash_to_streamlit.py
+++ TLCDriver_2024_01_dash_to_streamlit.py
@@ -6,7 +6,7 @@ df = pd.read_csv("TLCDriver_2024_01_dash.csv")
 heatmap_data = pd.DataFrame(df)
 
 # Streamlit app title
-st.title("Driver Pay Heatmap by Weekday and Hour"
+st.title("Driver Pay Heatmap by Weekday and Hour",
          page_icon=':earth_americas:')
 
 # Dropdown for weekday selection
