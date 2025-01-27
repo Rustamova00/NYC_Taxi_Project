c5f50375fe43a76dc8613faab2642401968c4dba
Rustamova00 <99622577+Rustamova00@users.noreply.github.com>
2025-01-26 19:15:03 -0500

    Delete TLCDriver_2024_01_dash_to_streamlit.py

--- TLCDriver_2024_01_dash_to_streamlit.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-df_tripdata_2024_01 = pd.read_csv("TLCDriver_2024_01_dash.csv")
-st.write(df_tripdata_2024_01.head())
